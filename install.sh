c5b51b817819c796041dd354276f81da4cd0a4c0
Thomas Charlat <thomas.charlat@gmail.com>
2015-06-25 17:02:28 +0200

    adapted script to new architecture

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-git clone http://github.com/Kallikrein/www.git cordova/www2 &&
+git clone http://github.com/Kallikrein/core.git cordova/core;
 git clone http://github.com/Kallikrein/instance.git cordova/instance
