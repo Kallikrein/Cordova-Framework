b3ec0bd91b30f823a2fb926c70317f3e7202a419
Thomas Charlat <thomas.charlat@gmail.com>
2015-06-25 16:58:11 +0200

    refactored framework architecture

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-git clone http://github.com/Kallikrein/www.git cordova/www &&
+git clone http://github.com/Kallikrein/www.git cordova/www2 &&
 git clone http://github.com/Kallikrein/instance.git cordova/instance
